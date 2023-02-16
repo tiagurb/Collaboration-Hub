8565378e05ec0a1fee84286ee1c648e7bab7d15f
tiagurb <tiagurb@hotmail.com>
2023-02-16 19:38:42 +0000

    sign up/in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import './App.css';
 import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <ToastContainer/>
+      <Navbar />
+      <Routes>
+        <Route path="/" />
+        <Route path="/projects" />
+        {/* <Route
+          path="/projects/add"
+          element={
+            // <IsPrivate>
+            //   <AddProject />
+            // </IsPrivate>
+          }
+        /> */}
+        {/* <Route path="/projects/:projectId" element={<ProjectDetail />} /> */}
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,54 @@
+import { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { login } from "../api";
+import { toast } from "react-toastify";
+import { UserContext } from "../context/user.context";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const { authenticateUser } = useContext(UserContext);
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  async function handleSubmitForm(event) {
+    event.preventDefault();
+    try {
+      const response = await login({ email, password });
+      localStorage.setItem("authToken", response.data);
+      // Setting the loged user in the context
+      authenticateUser();
+      toast.success("User logegd in");
+      navigate("/");
+    } catch (e) {
+      toast.error("Error occurred", e);
+    }
+  }
+
+  return (
+    <>
+      <h3>Login</h3>
+      <form>
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" onChange={handleEmailChange} />
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" onChange={handlePasswordChange} />
+
+        <button type="submit" onClick={handleSubmitForm}>
+          Sign up
+        </button>
+      </form>
+      <p>Don't have an account?</p>
+      <Link to="/signup">Signup</Link>
+    </>
+  );
+}
+
+export default Login;

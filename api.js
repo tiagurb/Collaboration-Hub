ef982ad5debbf98c005c07194ac70fd39e3596de
tiagurb <tiagurb@hotmail.com>
2023-02-16 19:29:39 +0000

    resolving the conflit

---
+++ api.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const BASE_URL = `${process.env.REACT_APP_PROJECTS_API}/api`;
+
+export const getAllProjects = () => {
+  return axios.get(`${BASE_URL}/project`);
+};
+
+export const getProject = (id) => {
+  return axios.get(`${BASE_URL}/project/${id}`);
+};
+
+export const deleteProject = (id) => {
+  return axios.delete(`${BASE_URL}/project/${id}`);
+};
+
+export const createProject = (project) => {
+  return axios.post(`${BASE_URL}/project`, project, {
+    headers: { Authorization: `Bearer ${localStorage.getItem("authToken")}`}
+  });
+};
+
+export const uploadImage = (uploadData) => {
+  return axios.post(`${BASE_URL}/upload`, uploadData);
+};
+
+export const login = (user) => {
+  return axios.post(`${BASE_URL}/login`, user);
+};
+
+export const signup = (user) => {
+  return axios.post(`${BASE_URL}/signup`, user);
+};
+
+export const verify = (token) => {
+  return axios.get(`${BASE_URL}/verify`, {
+    headers: { Authorization: `Bearer ${token}`},
+  });
+};
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import { NavLink } from "react-router-dom";
+import { UserContext } from "../context/user.context";
+import { useContext } from "react";
+
+function Navbar() {
+  const { loggedUser, logout } = useContext(UserContext);
+
+  return (
+    <>
+      <nav>
+        <div>
+          {loggedUser ? (
+            <div>
+              <p>Welcome {loggedUser.username}</p>
+              <div><NavLink to="/projects">Projects</NavLink></div>
+              <button onClick={logout}>Logout</button>
+            </div>
+          ) : (
+            <>
+              <div>
+                <NavLink to="/login">Login</NavLink>
+              </div>
+              <div>
+                <NavLink to="/signup">Signup</NavLink>
+              </div>
+            </>
+          )}
+        </div>
+        
+        
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;

6336d3f454176696711405d8949b197c4ec45a78
tiagurb <tiagurb@hotmail.com>
2023-02-18 11:32:25 +0000

    restore

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -9,67 +9,52 @@ function Signup() {
   const [username, setUsername] = useState("");
   const navigate = useNavigate();
 
-  function Signup() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [username, setUsername] = useState("");
-    const navigate = useNavigate();
-
-    function handleEmailChange(event) {
-      setEmail(event.target.value);
-    }
-
-    function handlePasswordChange(event) {
-      setPassword(event.target.value);
-    }
-
-    function handleUsernameChange(event) {
-      setUsername(event.target.value);
-    }
-
-    async function handleSubmitForm(event) {
-      event.preventDefault();
-      try {
-        const response = await signup({ email, password, username });
-        if (response.data.message) {
-          toast.info(response.data.message);
-        } else {
-          toast.success("User created");
-        }
-        navigate("/");
-      } catch (e) {
-        toast.error("Error occurred", e);
-      }
-    }
-
-    return (
-      <>
-        <h3>Sign up</h3>
-        <form>
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" onChange={handleEmailChange} />
-          <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="username"
-            onChange={handleUsernameChange}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            onChange={handlePasswordChange}
-          />
-
-          <button type="submit" onClick={handleSubmitForm}>
-            Sign up
-          </button>
-        </form>
-        <p>Already have an accout?</p>
-        <Link to="/login">Login</Link>
-      </>
-    );
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
   }
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleUsernameChange(event) {
+    setUsername(event.target.value);
+  }
+
+  async function handleSubmitForm(event) {
+    event.preventDefault();
+    try {
+      const response = await signup({ email, password, username });
+      if (response.data.message) {
+        toast.info(response.data.message);
+      } else {
+        toast.success("User created");
+      }
+      navigate("/");
+    } catch (e) {
+      toast.error("Error occurred", e);
+    }
+  }
+
+  return (
+    <>
+      <h3>Sign up</h3>
+      <form>
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" onChange={handleEmailChange} />
+        <label htmlFor="username">Username</label>
+        <input id="username" type="username" onChange={handleUsernameChange} />
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" onChange={handlePasswordChange} />
+
+        <button type="submit" onClick={handleSubmitForm}>
+          Sign up
+        </button>
+      </form>
+      <p>Already have an accout?</p>
+      <Link to="/login">Login</Link>
+    </>
+  );
 }
 
 export default Signup;

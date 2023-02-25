15cef0a0716928d8c80c262789b9e9b2d55c0ab4
João Mesquita <joaopsmesquita@gmail.com>
2023-02-25 15:55:21 +0000

    homepage hero section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -11,8 +11,36 @@ import {
   Icon,
   useColorModeValue,
   createIcon,
+  SimpleGrid,
+  Image,
+  Flex,
+  StackDivider,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+
+import {
+  IoAnalyticsSharp,
+  IoLogoBitcoin,
+  IoSearchSharp,
+} from "react-icons/io5";
+
+const Feature = ({ text, icon, iconBg }) => {
+  return (
+    <Stack direction={"row"} align={"center"}>
+      <Flex
+        w={8}
+        h={8}
+        align={"center"}
+        justify={"center"}
+        rounded={"full"}
+        bg={iconBg}
+      >
+        {icon}
+      </Flex>
+      <Text fontWeight={600}>{text}</Text>
+    </Stack>
+  );
+};
 
 function HomePage() {
   return (
@@ -23,7 +51,7 @@ function HomePage() {
           rel="stylesheet"
         />
       </Head>
-      <Container maxW={"3xl"}>
+      <Container maxW={"6xl"}>
         <Stack
           as={Box}
           textAlign={"center"}
@@ -85,6 +113,84 @@ function HomePage() {
             </Box>
           </Stack>
         </Stack>
+      </Container>
+      <Container maxW={"5xl"} py={12}>
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+          <Stack spacing={4}>
+            <Text
+              textTransform={"uppercase"}
+              color={"blue.400"}
+              fontWeight={600}
+              fontSize={"sm"}
+              bg={useColorModeValue("blue.50", "blue.900")}
+              p={2}
+              alignSelf={"flex-start"}
+              rounded={"md"}
+            >
+              Our Mission
+            </Text>
+            <Heading>
+              Keep track of all your tasks and projects in one place
+            </Heading>
+            <Text color={"gray.500"} fontSize={"lg"}>
+              Pro Tasker is the ultimate solution for individuals and teams who
+              need to stay organized and productive. With our powerful tool, you
+              can easily:
+            </Text>
+            <Stack
+              spacing={4}
+              divider={
+                <StackDivider
+                  borderColor={useColorModeValue("gray.100", "gray.700")}
+                />
+              }
+            >
+              <Feature
+                icon={
+                  <Icon
+                    as={IoAnalyticsSharp}
+                    color={"yellow.500"}
+                    w={5}
+                    h={5}
+                  />
+                }
+                iconBg={useColorModeValue("yellow.100", "yellow.900")}
+                text={"Create and manage tasks"}
+              />
+              <Feature
+                icon={
+                  <Icon as={IoLogoBitcoin} color={"green.500"} w={5} h={5} />
+                }
+                iconBg={useColorModeValue("green.100", "green.900")}
+                text={"Set deadlines"}
+              />
+              <Feature
+                icon={
+                  <Icon as={IoSearchSharp} color={"purple.500"} w={5} h={5} />
+                }
+                iconBg={useColorModeValue("purple.100", "purple.900")}
+                text={"Track progress"}
+              />
+              <Feature
+                icon={
+                  <Icon as={IoSearchSharp} color={"purple.500"} w={5} h={5} />
+                }
+                iconBg={useColorModeValue("purple.100", "purple.900")}
+                text={"Stay on top of everything and get things done!"}
+              />
+            </Stack>
+          </Stack>
+          <Flex>
+            <Image
+              rounded={"md"}
+              alt={"feature image"}
+              src={
+                "https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+              }
+              objectFit={"cover"}
+            />
+          </Flex>
+        </SimpleGrid>
       </Container>
     </>
   );
